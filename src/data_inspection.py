70dc14b92f8abb23f30998d6e6713cf4dca481c4
Peter Gormsen <peter.gormsen.dk@gmail.com>
2021-03-09 16:37:32 +0100

    dev

---
+++ src/data_inspection.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import pandas as pd
+
+columns_subset = ["loan_amnt",
+                  "term",
+                  "int_rate",
+                  "installment",
+                  "grade",
+                  "sub_grade",
+                  "emp_title",
+                  "emp_length",
+                  "home_ownership",
+                  "annual_inc",
+                  "issue_d",
+                  "loan_status",
+                  "purpose",
+                  "title",
+                  "addr_state",
+                  "dti",
+                  "earliest_cr_line",
+                  "fico_range_low",
+                  "fico_range_high",
+                  "total_acc",
+                  "total_pymnt"]
+
+df = pd.read_csv('../data/raw/accepted_2007_to_2018Q4.csv', nrows=1000, usecols=columns_subset)
+
+# Remove all "current" loans and create new feature called "default"
+df = df[df["loan_status"].isin(["Fully Paid", "Charged Off"])]
+df['default'] = [1 if string == "Charged Off" else 0 for string in df['loan_status']]
+df.drop(columns="loan_status", inplace=True)
+
+# Clean employment length: -1 for NaN, 0 for <1 year and 10 for 10+
+df['emp_length'].fillna(-1, inplace=True)
+df['emp_length'].replace("< 1 year", 0, inplace=True)
+df['emp_length'] = [int(str(string).split(" ")[0].split("+")[0]) for string in df['emp_length']]
+
+# Convert "term" to int number
+df['term'] = [int(str(string)[:3]) for string in df['term']]
+
+# Calculate return on investment as a percentage
+df['return_inv'] = df.eval("(total_pymnt / loan_amnt)**(1/(term / 12)) - 1") * 100
+
+# Convert "sub_grade" score to integers from 0 (A1) to 25 (C5)
+subgrade_sorted = sorted(np.unique(df["sub_grade"]))
+df['sub_grade'] = [subgrade_sorted.index(subgrade) for subgrade in df['sub_grade']]
+
+print(df[:1000].to_string())
+
+# df.to_csv('../data/processed/accepted_2007_to_2018Q4.csv', index=False)
